ff5daff8dd419f0ba69ae65a648a7c0e6adf3ac1
destin <pytel.dawid@gmail.com>
2015-12-29 21:24:33 +0100

    Make it possible to run IJ from maven with plugin installed

--- intellij-installer/src/main/groovy/getintellij.groovy
+++ intellij-installer/src/main/groovy/getintellij.groovy
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 import org.apache.commons.io.FileUtils
 import org.codehaus.plexus.archiver.UnArchiver
 
---
+++ maven-test-support-plugin/src/main/script/installplugin.groovy
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2014 Dawid Pytel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.apache.commons.configuration.PropertiesConfiguration
+import org.apache.commons.configuration.PropertiesConfigurationLayout
+
+def installDir = new File(properties.getProperty('intellij.install.dir'))
+
+def intellijPropertiesFile = new File(installDir, "bin/idea.properties")
+
+PropertiesConfiguration config = new PropertiesConfiguration();
+PropertiesConfigurationLayout layout = new PropertiesConfigurationLayout(config);
+layout.load(new InputStreamReader(new FileInputStream(intellijPropertiesFile)));
+
+config.setProperty("plugin.path", properties.getProperty('plugin.jar'));
+layout.save(intellijPropertiesFile.newWriter());
